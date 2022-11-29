8a67c0e706148c02ea28fb3b0039a57481456d9d
PrivateMartys <martyna.jakubowska94@gmail.com>
2022-11-29 09:33:56 +0100

    Added routing introduction

--- sr-app/src/app/app.module.ts
+++ sr-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroductionComponent } from './introduction/introduction.component';
 
+const appRoutes: Routes = [
+  { path: 'introduction', component: IntroductionComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroductionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
